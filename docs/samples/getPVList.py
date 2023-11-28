5a92299f0dd92e06f6a67e2e7dad51e1df3165dd
bustinduy <86886947+bustinduy@users.noreply.github.com>
2023-11-28 11:28:07 +0100

    Update getPVList.py to work better in python3
    - small chages to work in python3 (2023)
    |__ urllib2 -> urllib.request.urlopen
    |__ urllib.urlencode  -> urllib.parse.urlencode
    - correct tab spacing

--- docs/samples/getPVList.py
+++ docs/samples/getPVList.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-
+"""
+- small chages to work in python3 (2023)
+|__ urllib2 -> urllib.request.urlopen
+|__ urllib.urlencode  -> urllib.parse.urlencode
+- correct tab spacing
+"""
 import urllib
-import urllib2
+#import urllib2
 import json
 import sys
 
@@ -14,13 +19,13 @@ finalPVList = []
 applianceMGMTUrl = 'http://archiver.slac.stanford.edu:17665/mgmt/bpl/getAllPVs'
 if len(PVPatterns) > 0:
 	for pattern in PVPatterns:
-		resp = urllib2.urlopen(url=applianceMGMTUrl + "?" + urllib.urlencode({"pv" : pattern}))
-		matchingPVs = json.load(resp)
-		finalPVList.extend(matchingPVs)
+	resp = urllib.request.urlopen(url=applianceMGMTUrl + "?" + urllib.parse.urlencode({"pv" : pattern}))
+	matchingPVs = json.load(resp)
+	finalPVList.extend(matchingPVs)
 else:
-		resp = urllib2.urlopen(url=applianceMGMTUrl)
-		matchingPVs = json.load(resp)
-		finalPVList.extend(matchingPVs)
+	resp = urllib.request.urlopen(url=applianceMGMTUrl)
+	matchingPVs = json.load(resp)
+	finalPVList.extend(matchingPVs)
 	
 
 for pv in sorted(finalPVList):
